f7004e4117b6145dc9059aa409fe5b0858ceb9da
Thone <46010284+thonecardoso@users.noreply.github.com>
2022-05-12 21:33:15 -0300

    Criação do Projeto

---
+++ src/main/java/com/magicthegathering/MagicthegatheringApplication.java
@@ -0,0 +1,13 @@
+package com.magicthegathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicthegatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicthegatheringApplication.class, args);
+	}
+
+}
